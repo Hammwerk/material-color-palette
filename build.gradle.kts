948b3b176d4a9a773ada2b3f818985bd6ee029fb
David Hamm <david.hamm@hammwerk.de>
2021-05-18 08:37:56 +0200

    maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.0"
+    `maven-publish`
+    signing
 }
 
 group = "de.hammwerk"
@@ -7,6 +9,69 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "ossrh"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                val ossrhUsername: String? by project
+                val ossrhPassword: String? by project
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "material-color-palette"
+            version = project.version.toString()
+
+            pom {
+                packaging = "jar"
+                name.set("Material Design Color Palette Generator")
+                description.set("A reverse engineered library for generating a custom Material Design color palette.")
+                url.set("https://github.com/Hammwerk/material-color-palette")
+
+                scm {
+                    connection.set("scm:git:git://github.com/Hammwerk/material-color-palette.git")
+                    developerConnection.set("scm:git:ssh://github.com/Hammwerk/material-color-palette.git")
+                    url.set("https://github.com/Hammwerk/material-color-palette")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("David Hamm")
+                        email.set("david.hamm@hammwerk.de")
+                        organization.set("Hammwerk")
+                    }
+                }
+            }
+
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["maven"])
 }
 
 tasks.test {
